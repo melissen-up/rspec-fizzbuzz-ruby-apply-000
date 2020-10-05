358252baa195cfb899d12d011bd9c9db4022302d
Melissa F <melissa.faulner@gmail.com>
2020-10-05 20:45:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,9 @@
 def fizzbuzz(int)
+  
+  if int % 3 == 0 && int % 5 == 0
+     return "FizzBuzz"
+  end
+  
   if int % 3 == 0 
      return "Fizz" 
   end
@@ -6,10 +11,7 @@ def fizzbuzz(int)
   if int % 5 == 0
      return "Buzz"
   end
-  
-  if int % 3 == 0 && int % 5 == 0
-     return "FizzBuzz"
-  end
+
 end
 
 
